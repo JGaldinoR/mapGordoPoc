06cf2e1da6dc140086240b66931b69a20bb0f2d1
Jose Galdino Ramirez Alvarez <jgaldino_ramirez@hotmail.com>
2019-08-25 02:27:40 -0500

    getting conflicts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnChanges, AfterViewInit } from '@angular/core';
 import { UserService } from './user.service';
-
+//IF YOU GEY A CONFLICT IT IS BECAUSE YOU DONT KNOW GIT
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
